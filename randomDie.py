184f523b24e8a6c983d76a65291c7eaf84cb6e9f
Ice <Alex2cool4@gmail.com>
2022-07-02 14:14:05 -0400

    Python Commit

---
+++ randomDie.py
@@ -0,0 +1,11 @@
+import random
+# gives me a number between 1-20
+def randomD20():
+    print(random.randint(1,20))
+
+# gives me a number between 1-10
+def randomD10():
+    print(random.randint(1,10))
+
+randomD20()
+randomD10()
---
+++ start.py
@@ -0,0 +1,29 @@
+
+# uh i lost the hyperfocus on a dnd project in java and now im here
+# ill probably post it later but for now i dont like setting up modifiers and hash tables for all the races and classes nope
+# all the java is making me want to use brackets but no this is python we don't need that, instead its implied with indents. This is just like social cues, and like social cues i will never understand them.
+
+# Hello world but different
+def hallo():
+    print ("Hallo")
+
+# Squaring function
+def math():
+    num = 8
+    numSquare = (num * num)
+    print(numSquare)
+
+# prints a range of 1 through n where n is defined
+def printNum(n):
+    for b in range(n):
+        print(b+1)
+
+# using my knowledge of discrete math for bitwise operators, specifically shifts
+def shifts():
+    print(1011<<2)
+    print(1001>>2)
+
+shifts()
+hallo()
+math()
+printNum(5)
